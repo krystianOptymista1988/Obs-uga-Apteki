08d00627ed11e40fd13bf53182674a8163095b87
Krystian Czarnecki <krystian.czarneckipl@gmail.com>
2024-05-22 22:51:13 +0200

    Zmiany w PAtientAddEdit

---
+++ PatientAddEdit.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace Obsługa_Apteki
+{
+    partial class PatientAddEdit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PatientAddEdit));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tbId = new System.Windows.Forms.TextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbSurname = new System.Windows.Forms.TextBox();
+            this.tbPESEL = new System.Windows.Forms.TextBox();
+            this.tbMobile = new System.Windows.Forms.TextBox();
+            this.tbAdress = new System.Windows.Forms.TextBox();
+            this.tbPostalCode = new System.Windows.Forms.TextBox();
+            this.dtpDateOfBirth = new System.Windows.Forms.DateTimePicker();
+            this.tbComment = new System.Windows.Forms.RichTextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.cbPharmaceut = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 94);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Imię";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(324, 98);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nazwisko";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 154);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "PESEL";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 271);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Adres";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(338, 271);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Kod Pocztowy";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(324, 154);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Telefon";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(52, 211);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(121, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Data Urodzenia";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(234, 343);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(57, 20);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Uwagi:";
+            // 
+            // tbId
+            // 
+            this.tbId.Enabled = false;
+            this.tbId.Location = new System.Drawing.Point(88, 23);
+            this.tbId.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbId.Name = "tbId";
+            this.tbId.Size = new System.Drawing.Size(84, 26);
+            this.tbId.TabIndex = 9;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(88, 89);
+            this.tbName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(180, 26);
+            this.tbName.TabIndex = 10;
+            // 
+            // tbSurname
+            // 
+            this.tbSurname.Location = new System.Drawing.Point(412, 94);
+            this.tbSurname.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbSurname.Name = "tbSurname";
+            this.tbSurname.Size = new System.Drawing.Size(163, 26);
+            this.tbSurname.TabIndex = 11;
+            // 
+            // tbPESEL
+            // 
+            this.tbPESEL.Location = new System.Drawing.Point(88, 149);
+            this.tbPESEL.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbPESEL.Name = "tbPESEL";
+            this.tbPESEL.Size = new System.Drawing.Size(180, 26);
+            this.tbPESEL.TabIndex = 12;
+            // 
+            // tbMobile
+            // 
+            this.tbMobile.Location = new System.Drawing.Point(412, 149);
+            this.tbMobile.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbMobile.Name = "tbMobile";
+            this.tbMobile.Size = new System.Drawing.Size(163, 26);
+            this.tbMobile.TabIndex = 13;
+            // 
+            // tbAdress
+            // 
+            this.tbAdress.Location = new System.Drawing.Point(100, 266);
+            this.tbAdress.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbAdress.Name = "tbAdress";
+            this.tbAdress.Size = new System.Drawing.Size(212, 26);
+            this.tbAdress.TabIndex = 14;
+            // 
+            // tbPostalCode
+            // 
+            this.tbPostalCode.Location = new System.Drawing.Point(459, 266);
+            this.tbPostalCode.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbPostalCode.Name = "tbPostalCode";
+            this.tbPostalCode.Size = new System.Drawing.Size(145, 26);
+            this.tbPostalCode.TabIndex = 15;
+            // 
+            // dtpDateOfBirth
+            // 
+            this.dtpDateOfBirth.Location = new System.Drawing.Point(200, 211);
+            this.dtpDateOfBirth.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dtpDateOfBirth.Name = "dtpDateOfBirth";
+            this.dtpDateOfBirth.Size = new System.Drawing.Size(319, 26);
+            this.dtpDateOfBirth.TabIndex = 16;
+            // 
+            // tbComment
+            // 
+            this.tbComment.Location = new System.Drawing.Point(33, 368);
+            this.tbComment.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbComment.Name = "tbComment";
+            this.tbComment.Size = new System.Drawing.Size(625, 253);
+            this.tbComment.TabIndex = 17;
+            this.tbComment.Text = "";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnAdd.Location = new System.Drawing.Point(266, 685);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(184, 46);
+            this.btnAdd.TabIndex = 18;
+            this.btnAdd.Text = "Akceptuj";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Crimson;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnDelete.Location = new System.Drawing.Point(488, 685);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(184, 46);
+            this.btnDelete.TabIndex = 19;
+            this.btnDelete.Text = "Anuluj";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // cbPharmaceut
+            // 
+            this.cbPharmaceut.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cbPharmaceut.FormattingEnabled = true;
+            this.cbPharmaceut.Location = new System.Drawing.Point(236, 631);
+            this.cbPharmaceut.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cbPharmaceut.Name = "cbPharmaceut";
+            this.cbPharmaceut.Size = new System.Drawing.Size(210, 32);
+            this.cbPharmaceut.TabIndex = 20;
+            this.cbPharmaceut.SelectedIndexChanged += new System.EventHandler(this.cbPharmaceut_SelectedIndexChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(40, 637);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(184, 20);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "Farmaceuta Prowadzący";
+            // 
+            // PatientAddEdit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 766);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.cbPharmaceut);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tbComment);
+            this.Controls.Add(this.dtpDateOfBirth);
+            this.Controls.Add(this.tbPostalCode);
+            this.Controls.Add(this.tbAdress);
+            this.Controls.Add(this.tbMobile);
+            this.Controls.Add(this.tbPESEL);
+            this.Controls.Add(this.tbSurname);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.tbId);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "PatientAddEdit";
+            this.Text = "Dodaj lub Edytuj Pacjenta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox tbId;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbSurname;
+        private System.Windows.Forms.TextBox tbPESEL;
+        private System.Windows.Forms.TextBox tbMobile;
+        private System.Windows.Forms.TextBox tbAdress;
+        private System.Windows.Forms.TextBox tbPostalCode;
+        private System.Windows.Forms.DateTimePicker dtpDateOfBirth;
+        private System.Windows.Forms.RichTextBox tbComment;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.ComboBox cbPharmaceut;
+        private System.Windows.Forms.Label label10;
+    }
+}
---
+++ PatientAddEdit.cs
@@ -0,0 +1,161 @@
+﻿using Obsługa_Apteki.Entities;
+using Obsługa_Apteki.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace Obsługa_Apteki
+{
+    public partial class PatientAddEdit : Form
+    {
+        public int _patientId;
+        private string pesel;
+        private AptekaTestDbContext _context;
+        public Patient patient;
+        public int _pharmaceutId;
+        List<Pharmaceut> _pharmaceuts;
+
+
+        public PatientAddEdit(string patientId, AptekaTestDbContext context)
+        {
+            InitializeComponent();
+            _context = context;
+            LoadComboboxData();
+            pesel = patientId;
+            GetPatientData();
+        }
+        public PatientAddEdit()
+        {
+            InitializeComponent();
+            _context = new AptekaTestDbContext();
+            LoadComboboxData();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+
+                if (!string.IsNullOrEmpty(tbId.Text))
+                {
+                    // var existingPatient = _context.Patients.SingleOrDefault(p => p.PatientId == patient.PatientId);
+                    _context.Patients.Attach(patient);
+                    patient = CreatePatient(patient);
+                    _context.Entry(patient).State = EntityState.Modified;
+                    MessageBox.Show("Aktualizowano dane Pacjenta");
+                }
+                else
+                {
+                    patient = new Patient();
+                    patient = CreatePatient();
+                    _context.Entry(patient);
+                    MessageBox.Show(patient.PatientId.ToString() + patient.Name.ToString() );
+                    _context.Patients.Add(patient);
+                }
+                _context.SaveChanges();
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Błąd podczas zapisywania danych: " + ex.Message);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            PatientShow patientShowForm = new PatientShow();
+            Close();
+        }
+
+        private void GetPatientData()
+        {
+            if (pesel != null)
+            {
+                Text = "Edytowanie danych Pacjenta";
+                patient = _context.Patients.FirstOrDefault(x => x.PESEL == pesel);
+
+                if (patient == null)
+                {
+                    throw new Exception("Brak użytkownika o podanym Id");
+                }
+
+                FillTextBoxes(patient);
+            }
+        }
+        private void FillTextBoxes(Patient patient)
+        {
+
+            tbId.Text = patient.PatientId.ToString();
+            tbName.Text = patient.Name;
+            tbSurname.Text = patient.Surname;
+            tbAdress.Text = patient.Adress;
+            tbComment.Text = patient.Comment;
+            tbMobile.Text = patient.Mobile;
+            tbPESEL.Text = patient.PESEL;
+            tbPostalCode.Text = patient.PostalCode;
+            dtpDateOfBirth.Text = patient.DateOfBirth.ToString();
+
+        }
+
+        private Patient CreatePatient()
+        {
+            int pharmaceutID = int.Parse(cbPharmaceut.SelectedValue.ToString());
+
+            patient.Name = tbName.Text;
+            patient.Surname = tbSurname.Text;
+            patient.DateOfBirth = DateTime.Parse(dtpDateOfBirth.Text);
+            patient.PESEL = tbPESEL.Text;
+            patient.Adress = tbAdress.Text;
+            patient.PostalCode = tbPostalCode.Text;
+            patient.Mobile = tbMobile.Text;
+            patient.Comment = tbComment.Text;
+            patient.PharmaceutId = int.Parse(cbPharmaceut.SelectedValue.ToString());
+            patient.Pharmaceut = GetPharmaceutFromId(pharmaceutID);
+            MessageBox.Show(patient.PatientId.ToString());
+            return patient;
+        }
+
+        private Patient CreatePatient(Patient id)
+        {
+            int pharmaceutID = int.Parse(cbPharmaceut.SelectedValue.ToString());
+
+            patient.Name = tbName.Text;
+            patient.Surname = tbSurname.Text;
+            patient.DateOfBirth = DateTime.Parse(dtpDateOfBirth.Text);
+            patient.PESEL = tbPESEL.Text;
+            patient.Adress = tbAdress.Text;
+            patient.PostalCode = tbPostalCode.Text;
+            patient.Mobile = tbMobile.Text;
+            patient.Comment = tbComment.Text;
+            patient.PharmaceutId = int.Parse(cbPharmaceut.SelectedValue.ToString());
+            patient.Pharmaceut = GetPharmaceutFromId(pharmaceutID);
+            MessageBox.Show(patient.PharmaceutId.ToString());
+            id = patient;
+            return id;
+        }
+
+        private void cbPharmaceut_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void LoadComboboxData()
+        {
+            _pharmaceuts = _context.Pharmaceuts.ToList();
+            cbPharmaceut.DataSource = _pharmaceuts;
+            cbPharmaceut.DisplayMember = "FullName";
+            cbPharmaceut.ValueMember = "PharmaceutId";
+        }
+        public Pharmaceut GetPharmaceutFromId(int pharmaceutId)
+        {
+            return _context.Pharmaceuts.Find(pharmaceutId);
+        }
+    }
+
+}
